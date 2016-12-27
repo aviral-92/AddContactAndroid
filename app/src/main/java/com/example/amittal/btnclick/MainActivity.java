3400c7ae2c49b2c5db5958ab88d28f7e7eb18f5a
Anil Rautela <arautela@xavient.com>
2016-12-27 17:35:04 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/amittal/btnclick/MainActivity.java
@@ -0,0 +1,182 @@
+package com.example.amittal.btnclick;
+
+import java.util.ArrayList;
+
+import android.Manifest;
+import android.content.ContentProviderOperation;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int PICK_CONTACT_REQUEST = 1;
+    private int STORAGE_PERMISSION_CODE = 1;
+    private Cursor crContacts;
+
+    private TextView mResult;
+    private String mEmail;
+
+    private String mRawContactId;
+    private String mDataId;
+    private EditText name;
+    private EditText mobile;
+    Button btnClickMe;
+    String Fname = null;
+    String Mob = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnClickMe = (Button) findViewById(R.id.button);
+        name = ((EditText) findViewById(R.id.email));
+        mobile = ((EditText) findViewById(R.id.Mobile));
+        btnClickMe.setOnClickListener(MainActivity.this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        TextView tv = (TextView) findViewById(R.id.textView2);
+        tv.setText("Aviral");
+        Toast.makeText(getBaseContext(), "You Pressed Me...!", Toast.LENGTH_SHORT).show();
+        if(isReadStorageAllowed()){
+            //If permission is already having then showing the toast
+            Toast.makeText(MainActivity.this,"You already have the permission",Toast.LENGTH_LONG).show();
+            //ContentResolver contactAdder = new
+            //crContacts = getContactCursor(getContentResolver(), "");
+            //crContacts.moveToFirst();
+            insertContact(getContentResolver(), getName().getText().toString(), getMobile().getText().toString());
+            //Existing the method with return
+            return;
+        }
+        requestStoragePermission();
+        //mEmail = mEmailEditText.getText().toString();
+        //startActivityForResult(new Intent(Intent.ACTION_PICK, Contacts.CONTENT_URI), PICK_CONTACT_REQUEST);
+
+    }
+
+    public static Cursor getContactCursor(ContentResolver contactHelper,
+                                          String startsWith) {
+
+        String[] projection = { ContactsContract.CommonDataKinds.Phone._ID,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER };
+        Cursor cur = null;
+
+        try {
+            if (startsWith != null && !startsWith.equals("")) {
+                cur = contactHelper.query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        projection,
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
+                                + " like \"" + startsWith + "%\"", null,
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
+                                + " ASC");
+            } else {
+                cur = contactHelper.query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        projection, null, null,
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
+                                + " ASC");
+            }
+            cur.moveToFirst();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return cur;
+    }
+
+    private boolean isReadStorageAllowed() {
+        //Getting the permission status
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CONTACTS);
+
+        //If permission is granted returning true
+        if (result == PackageManager.PERMISSION_GRANTED)
+            return true;
+        //If permission is not granted returning false
+        return false;
+    }
+
+    private void requestStoragePermission(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.WRITE_CONTACTS)){
+            //If the user has denied the permission previously your code will come to this block
+            //Here you can explain why you need this permission
+            //Explain here why you need this permission
+        }
+
+        //And finally ask for the permission
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_CONTACTS},STORAGE_PERMISSION_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        //Checking the request code of our request
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+
+            //If permission is granted
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                //Displaying a toast
+                Toast.makeText(this, "Permission granted now you can Write", Toast.LENGTH_LONG).show();
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(this, "Oops you just denied the permission", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+//    .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER,mobileNumber).withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)
+
+    public boolean insertContact(ContentResolver contactAdder, String firstName, String mobileNumber) {
+
+        ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
+        ops.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI).withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null).withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null).build());
+        ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI).withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0).withValue(ContactsContract.Data.MIMETYPE,ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE).withValue(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME,firstName).build());
+        ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI).withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0).withValue(ContactsContract.Data.MIMETYPE,ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE).withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, mobileNumber).withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE).build());
+        try {
+            contactAdder.applyBatch(ContactsContract.AUTHORITY, ops);
+        } catch (Exception e) {
+            MainActivity activity = new MainActivity();
+            Toast.makeText(activity, "In Catch Block", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+            return false;
+        }
+        MainActivity activity = new MainActivity();
+        //Toast.makeText(activity, "returning True", Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+    public EditText getName() {
+        return name;
+    }
+
+    public void setName(EditText name) {
+        this.name = name;
+    }
+
+    public EditText getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(EditText mobile) {
+        this.mobile = mobile;
+    }
+}
